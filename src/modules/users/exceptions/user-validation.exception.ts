1161f07f449a3afc6f2f5c645176678f33c6aeff
Alexey Kap <alexey.octopus.2020@gmail.com>
2023-08-11 13:33:03 +0300

    fix(develop): fix

--- src/modules/users/exceptions/user-validation.exception.ts
+++ src/modules/users/exceptions/user-validation.exception.ts
@@ -1,7 +1,8 @@
 import { BadRequestException } from '@nestjs/common';
+import { ValidationErrorItem } from 'sequelize/types/errors/validation-error';
 
 export class UserValidationException extends BadRequestException {
-  constructor(error) {
+  constructor(error: ValidationErrorItem[]) {
     const ValidationErrorItem = error[0];
     super({ message: ValidationErrorItem.message });
   }
--- src/modules/users/transforms/user.transform.ts
+++ src/modules/users/transforms/user.transform.ts
@@ -1,7 +1,7 @@
 import { User } from '../../../models/user.model';
 import { GetUserHandlers } from '../handlers/get-user';
 
-export function userTransform(user: User) {
+export function userTransform(user: User | null) {
   if (!user) return null;
   return new GetUserHandlers(user);
 }
